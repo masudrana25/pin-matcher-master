3575531b99c2cc194cc6b1862cd4320e6beb858a
masudrana25 <mdmasudrana2512000@gmail.com>
2023-06-07 20:04:18 +0600

    fdsfds

--- srcipt.js
+++ srcipt.js
@@ -1,7 +1,21 @@
 const inputGeneratePin = document.getElementById("inputGeneratePin");
+const numberButton = document.getElementById("button_number");
+
+
 document.getElementById("generatePin").addEventListener("click", () => GeneratePin());
 
 function GeneratePin() {
   const pin = Math.round(Math.random() * 1000000);
   inputGeneratePin.value = pin;
-}
+};
+
+let myPin = [];
+document.getElementById("number").addEventListener("click", function (event) {
+  const number = event.target.innerText;
+  const value = parseInt(number)
+  if (value || value === 0) {
+    myPin.push(value);
+  };
+  const finalPin= myPin.join('');
+  document.getElementById("pinShow").value = finalPin;
+});

07c0413a5964699c55261fd01215e545b6774cbe
masudrana25 <mdmasudrana2512000@gmail.com>
2023-06-07 21:19:18 +0600

    fggfd

--- srcipt.js
+++ srcipt.js
@@ -1,21 +1,37 @@
-const inputGeneratePin = document.getElementById("inputGeneratePin");
-const numberButton = document.getElementById("button_number");
+let inputGeneratedPin = document.getElementById("inputGeneratedPin");
 
-
+//Generate the pin number
 document.getElementById("generatePin").addEventListener("click", () => GeneratePin());
-
 function GeneratePin() {
   const pin = Math.round(Math.random() * 1000000);
   inputGeneratePin.value = pin;
 };
+let myPin = '';
+document.getElementById("input_text").addEventListener("click", function (event) {
+  let typing_pin = document.getElementById("typing_pin");
+  let pinValue = typing_pin.value;
+  const input = event.target.innerText;
 
-let myPin = [];
-document.getElementById("number").addEventListener("click", function (event) {
-  const number = event.target.innerText;
-  const value = parseInt(number)
-  if (value || value === 0) {
-    myPin.push(value);
+  if (isNaN(input)) {
+
+    if (input === 'C') {
+      typing_pin.value = '';
+      myPin = '';
+    };
+
+    if (input === '<') {
+      const oldPin = pinValue.split('');
+      oldPin.pop();
+      const newPin = oldPin.join('');
+      myPin = newPin;
+      typing_pin.value = newPin;
+    };
+  }
+  
+  else {
+    const newPin = pinValue + input;
+    typing_pin.value = newPin;
   };
-  const finalPin= myPin.join('');
-  document.getElementById("pinShow").value = finalPin;
+
 });
+
